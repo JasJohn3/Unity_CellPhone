881140227e12deb49cd8cf5f5dcebb9e5cf9a30b
Jason Lane Johnson <jasjohn3@uat.edu>
2019-10-21 21:26:35 -0700

    Mouse Switch Mode

--- Unity_Cell_Phone_UI/Assets/Scenes/Cell Phone Assets/Scripts/MouseSwitcher.cs
+++ Unity_Cell_Phone_UI/Assets/Scenes/Cell Phone Assets/Scripts/MouseSwitcher.cs
@@ -7,11 +7,27 @@ public class MouseSwitcher : MonoBehaviour
 {
     public RigidbodyFirstPersonController controller;
     // Update is called once per frame
+    public GameObject cellPhone;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
             controller.mouseLook.SetCursorLock(!controller.mouseLook.lockCursor);
+            if (controller.mouseLook.lockCursor == true)
+            {
+                cellPhone.SetActive(false);
+            }
+            else
+            {
+                toggleObject(cellPhone);
+            }
+            
+
+
         }    
+    }
+    public void toggleObject(GameObject objectToToggle)
+    {
+        objectToToggle.SetActive(!objectToToggle.activeInHierarchy);
     }
 }
